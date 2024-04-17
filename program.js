6e67862b0ec54c926e7341addd410e62c366ac43
Januda Bethmin <desilvabethmin@gmail.com>
2024-04-17 20:05:44 +0530

    spinning the slot machine done

--- program.js
+++ program.js
@@ -20,9 +20,31 @@
 // This is more like a defining a new function called prompt using the prompt-sync module
 const prompt = require("prompt-sync")();
 
+// We normally declare our gloabl variables at the top of the file (after the imports and the libraries)
+// This is a good practice because it is easy to see them and change them if needed
 // We need to define the number of ROW and COLUMN in the slot machine
-const ROW = 3;
-const COLUMN = 3;
+const ROWS = 3;
+const COLS = 3;
+
+// We need to define the symbols that can appear in the slot machine
+// We can create a map for that
+// Map is simillar to a dictionary in Python
+// It holds key-value pairs
+const SYMBOLS_COUNT = {
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
+
+// We need to define the multiplier for each symbol
+// We can create a map for that too
+const SYMBOLS_VALUE = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+};
 
 // Creating a function to deposit money
 const depositMoney = () => {
@@ -81,6 +103,78 @@ const getBet = (balance, numberOfLines) => {
   }
 };
 
+// Now we need a method to spin the slot machine
+const spin = () => {
+  // We can put all the of the possible symbols in an array
+  // We can use const but still manipulate a the data in a array (adding and removing elements)
+  // That's because we are not changing the reference of the array
+  // We are changing the data inside the array
+  // Because of this array is considered as a reference data type
+  const symbols = [];
+  // This is gonna loop through the SYMBOLS_COUNT map
+  // And give us the symbol and the count of the symbol per iteration
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    // This is gonna loop through the count of the symbol
+    // We are looping through the count of each symbol
+    for (let i = 0; i < count; i++) {
+      // This is gonna push the symbol to the symbols array
+      symbols.push(symbol);
+    }
+  }
+
+  // Now we have the symbols array with all the symbols
+  // We can now have a nested array to represent the slot machine
+  // You need to show the internal arrays inside the array like this
+  const reels = [[], [], []];
+
+  // reels = [ [A, B, C], [D, A, B], [C, D, A] ]
+  // reel1 = [ A, B, C ]
+  // reel2 = [ D, A, B ]
+  // reel3 = [ C, D, A ]
+
+  // There are the 3 different reels in the slot machine
+  // The actual rows will be like
+  // row1 = A D C
+  // row2 = B A D
+  // row3 = C B A
+
+  // Looping through every single reels
+  for (let i = 0; i < COLS; i++) {
+    // We need to have a copy of the symbols array per reel
+    // Because we need to shuffle the symbols array and we have to remove the selected symbols from the array to avoid duplicates
+    // This is called a deep copy
+    // We can use the spread operator to create a deep copy of the array
+    // This is gonna create a new array with the same elements
+    // This is not gonna create a new reference to the array
+    // So the main array won't be affected by the changes in the new array
+    const reelSymbols = [...symbols];
+    // Seeting the symbols for all the rows per reels
+    for (let j = 0; j < ROWS; j++) {
+      // Generating a random index number
+      // Math.random -> Generates a random number between 0 and 1
+      // Math.random * reelSymbols.length -> Generates a random number between 0 and the length of the reelSymbols array
+      // Math.floor -> Rounds the number to the nearest integer (downwards)
+      // We can't rownd up because because we might get a value that is greater than the length of the reelSymbols array
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+      //Selecting a random symbol from the reelSymbols array
+      const selectedSymbol = reelSymbols[randomIndex];
+
+      // Pushing into internal reel arrays (reel1, reel2, reel3)
+      reels[i].push(selectedSymbol);
+
+      // Removing the selected symbol from the reelSymbols array
+      // We can use the splice() method to remove an element from an array
+      // splice() -> Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
+      // We need to pass the index of the element we want to remove and the number of elements we want to remove
+      // We only want to remove 1 element
+      reelSymbols.slice(randomIndex, 1);
+    }
+  }
+  return reels;
+};
+
+const reels = spin();
+console.log(reels);
 // We need to define this variable using let instead const becuase we need to change the value of this variable
 // const -> Can't change the value of the variable
 // let -> Can change the value of the variable
